6971555d56a6d67466d4e920d7a0309016117d90
Gokul Soumya <gokulps15@gmail.com>
2020-12-19 20:25:08 +0530

    Formatting changes

--- spambot.py
+++ spambot.py
@@ -1,10 +1,11 @@
-import pyautogui
 import time
+import pyautogui
+
 time.sleep(3)                      # Delayes the starting
 
 message = "spam message"           # Message to be delivered
 repeat = 10                        # Number of time to be repeated
 
-for i in range(repeat):                
+for i in range(repeat):
     pyautogui.typewrite(message)
     pyautogui.press("enter")
