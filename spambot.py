70f7f548317cb9eddcbe18326597cfb8c10fb585
Govind <govinddevika4@gmail.com>
2020-12-19 18:09:26 +0530

    Initial upload

---
+++ spambot.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+time.sleep(3)                      # Delayes the starting
+
+message = "spam message"           # Message to be delivered
+repeat = 10                        # Number of time to be repeated
+
+for i in range(repeat):                
+    pyautogui.typewrite(message)
+    pyautogui.press("enter")
